169511cce2245efb4325e83afb7d3dd916198f47
Michał Kwiecień <michalkwiecien@MacBook-Pro-Micha.local>
2017-04-30 12:36:59 +0200

    Dashboard UI

--- v2/reflekto/AppDelegate.swift
+++ v2/reflekto/AppDelegate.swift
@@ -15,6 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
     var navigationController: REFNavigationController!
+    var bluetoothManager: BluetoothManager!
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
--- v2/reflekto/DashboardViewController.swift
+++ v2/reflekto/DashboardViewController.swift
@@ -11,23 +11,62 @@ import RxCocoa
 import RxSwift
 
 class DashboardViewController: BaseViewController {
-
+    
     override class func storyboardName() -> String? {
         return "Dashboard"
     }
     
+    var appDelegate: AppDelegate {
+        return UIApplication.shared.delegate as! AppDelegate
+    }
+    
+    @IBOutlet weak var helloLabel: UILabel!
+    @IBOutlet weak var statusBackground: UIView!
+    @IBOutlet weak var statusText: UILabel!
+    @IBOutlet weak var turnOffButton: UIButton!
+    @IBOutlet weak var turnOnButton: UIButton!
     @IBOutlet weak var setupButton: UIButton!
     
     override func initializeView() {
+        turnOnButton.rx.tap
+            .subscribe(onNext: { [unowned self] in
+                self.appDelegate.bluetoothManager = BluetoothManager()
+                self.updateBluetoothManagerStatus()
+            })
+            
+        .addDisposableTo(disposeBag)
+        
+        turnOffButton.rx.tap
+            .subscribe(onNext: { [unowned self] in
+                self.appDelegate.bluetoothManager = nil
+                self.updateBluetoothManagerStatus()
+            })
+        .addDisposableTo(disposeBag)
+        
         setupButton.rx.tap
             .subscribe(onNext: { [unowned self] in
                 self.navigateToSetup()
             })
-            .addDisposableTo(disposeBag)
+        .addDisposableTo(disposeBag)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        helloLabel.text = "Hello, \(Configuration.name.value)"
+        updateBluetoothManagerStatus()
+    }
+    
+    private func updateBluetoothManagerStatus() {
+        changeStatusTo(turnedOn: appDelegate.bluetoothManager != nil)
     }
     
     private func navigateToSetup() {
         navigationController?.presentModallyNavigationVCWithViewController(withVCType: SetupInfoViewController.self, animated: true, popCurrentFromStack: true)
+    }
+    
+    private func changeStatusTo(turnedOn isTurnedOn: Bool) {
+        statusBackground.backgroundColor = isTurnedOn ? .refGreen : .refRed
+        statusText.text = isTurnedOn ? "Working" : "Not working"
     }
 
 }
